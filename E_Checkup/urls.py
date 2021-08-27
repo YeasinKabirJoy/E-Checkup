e706991178024216908932a74bc72aa96cba322f
Nafees Imtiaz <67729890+NafeesImtiaz19@users.noreply.github.com>
2021-08-28 00:39:01 +0600

    navbar

--- E_Checkup/urls.py
+++ E_Checkup/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from userManagement import views as userView
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',userView.viewHomePage, name='home'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('registration/', userView.registration, name='registration'),
 
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
