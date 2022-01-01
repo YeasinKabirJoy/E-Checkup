d1604d3bf8118dc430dcb0283f63e3fbc8552c54
Yeasin Kabir Joy <52117661+YeasinKabirJoy@users.noreply.github.com>
2022-01-02 04:42:37 +0600

    Revert "production"
    This reverts commit f55f8b4c78f6bc0e8d2888a6f64e3651faee6298.

--- E_Checkup/settings.py
+++ E_Checkup/settings.py
@@ -156,7 +156,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
