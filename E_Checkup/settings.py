404bc6677cd9ab6f29f9acc067cdd34f3b779933
Yeasin Kabir Joy <52117661+YeasinKabirJoy@users.noreply.github.com>
2022-01-03 00:09:17 +0600

    Update settings.py

--- E_Checkup/settings.py
+++ E_Checkup/settings.py
@@ -163,7 +163,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
