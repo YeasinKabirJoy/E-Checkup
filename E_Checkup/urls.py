019602b855bf7b3e131fcd366a8fb737b2d6d980
Yeasin Kabir Joy <52117661+YeasinKabirJoy@users.noreply.github.com>
2022-01-03 01:41:09 +0600

    Update urls.py

--- E_Checkup/urls.py
+++ E_Checkup/urls.py
@@ -20,11 +20,8 @@ from video import views as videoView
 from django.conf.urls.static import static
 from django.conf import settings
 from django.views.static import serve
-from django.conf.urls import url
 
 urlpatterns = [
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     path('admin/', admin.site.urls),
     path('',userView.viewHomePage, name='home'),
     path('accounts/', include('django.contrib.auth.urls')),
